38cb54997140b16635174bca9a0c14229c5c14e6
Liam Martens <hi@liammartens.com>
2025-11-04 07:52:17 -0500

    fix: ldflags

--- go-app/windows_build.ps1
+++ go-app/windows_build.ps1
@@ -1,4 +1,4 @@
 Write-Host "Building version: $env:VERSION"
 $env:CGO_CFLAGS="-I$PWD/vcpkg_installed/x64-windows/include"
-$env:CGO_LDFLAGS="-L$PWD/vcpkg_installed/x64-windows/lib -lSDL2 -X 'main.VERSION=$env:VERSION'"
-wails build
+$env:CGO_LDFLAGS="-L$PWD/vcpkg_installed/x64-windows/lib -lSDL2"
+wails build -ldflags "-X 'main.VERSION=$env:VERSION'"
